5e7a09026594fa1f75caf611aa4cb3580d92742d
서상진 <seosangjin@seosangjin-ui-MacBookPro.local>
2020-06-09 23:37:27 +0900

    first commit

---
+++ create_frame.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()  # 초기화
+
+screen_width = 480
+screen_height = 640
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption('nado_game')
